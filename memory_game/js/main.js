934fc5a7b6bd7885ad3ed77a0fd9439ebe97f6ce
unknown <lopezfabrizio@ymail.com>
2019-11-13 10:47:06 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,16 +1,24 @@
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
 
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
+function checkForMatch() {
 
-var cardTwo = cards[1];
-cardsInPlay.push(cardTwo);
-
-if (cardsInPlay.length === 2) {
-	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You Found a Match!");
-	} else {
-		alert("Sorry, try again.");
+	if (cardsInPlay.length === 2) {
+		if (cardsInPlay[0] === cardsInPlay[1]) {
+			alert("You Found a Match!");
+		} else {
+			alert("Sorry, try again.");
+		}
 	}
+
 }
+
+function flipCard(cardId) {
+
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId])
+}
+
+flipCard(0);
+flipCard(2);
+checkForMatch();
