cb98049f58ceb6a6bc9cc648ba613281ea24a832
unknown <lopezfabrizio@ymail.com>
2019-11-13 20:45:07 -0600

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -24,8 +24,8 @@ cardImage: "images/king-of-diamonds.png"
 var cardsInPlay = [];
 
 function checkForMatch() {
-
 	if (cardsInPlay.length === 2) {
+		this.setAttribute("src", cards[cardId].cardImage);
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You Found a Match!");
 		} else {
@@ -33,16 +33,29 @@ function checkForMatch() {
 		}
 	}
 
-}
+};
 
-function flipCard(cardId) {
-
+function flipCard() {
+	var cardId = this.getAttribute("data-id");
+	
 	console.log("User flipped " + cards[cardId].rank);
 	console.log(cards[cardId].cardImage);
 	console.log(cards[cardId].suit);
-	cardsInPlay.push(cards[cardId].rank)
-}
+	cardsInPlay.push(cards[cardId].rank);
+};
 
-flipCard(0);
-flipCard(2);
+function createBoard() {
+	for (let i = 0; i < cards.length; i ++) {
+		var cardElement = document.createElement("img");
+		cardElement.setAttribute("src", "images/back.png");
+		cardElement.setAttribute("data-id", i);
+		cardElement.addEventListener("click", flipCard);
+		document.getElementById("game-board").appendChild(cardElement);
+	}
+};
+
+createBoard();
 checkForMatch();
+
+
+
